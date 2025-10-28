2427878689a86966384414bf2a3d998c7e901c96
Angela Aleksovska <108076667+angelaaleksovska@users.noreply.github.com>
2025-10-28 16:32:49 +0100

    Initial html/css for internship training project

--- frontend-training/phase-3-js/weather/js/script.js
+++ frontend-training/phase-3-js/weather/js/script.js
@@ -24,6 +24,14 @@ searchBtn.addEventListener('click', () => {
     }
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+    const lastCity = localStorage.getItem('lastCity');
+
+    if(lastCity){
+        getWeather(lastCity)
+    }
+})
+
 async function getWeather(city){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
     console.log("URL:", url);
